d76173293c429b67830cbb12fc97c81ff65f226d
abdelwahabram <abdelwahab2000r@gmail.com>
2023-02-08 17:39:54 +0200

    added hash() bijective function and the reversed function

--- ss/views.py
+++ ss/views.py
@@ -5,3 +5,24 @@ from django.shortcuts import render
 
 def home(request):
     return HttpResponse("welcome")
+
+def hash(id):
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    shortUrl = ""
+    while int(id):
+        char = int(id%62)
+        shortUrl = alphabet[char] + shortUrl
+        id /= 62
+    return shortUrl
+
+def getId(shortUrl):
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    id = 0
+    base = 62
+    reversedShortUrl = shortUrl[::-1]
+    for idx, char in enumerate(reversedShortUrl):
+        num = alphabet.index(char)
+        id += num * pow(base, idx)
+    return id
+# print(hash(12345))
+# print(getId("dnh"))
