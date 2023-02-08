e0f6e14ab9377d6ac4cbd464d54156c4be7926e0
abdelwahabram <abdelwahab2000r@gmail.com>
2023-02-08 17:41:16 +0200

    added shorten view

--- ss/views.py
+++ ss/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import UrlForm
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("welcome")
+
+def shorten(request):
+    if request.method == "POST":
+        urlForm = UrlForm(request.POST)
+        if urlForm.is_valid():
+            # ToDo: check security
+            shortUrl = urlForm.save()
+            id = shortUrl.id
+            return render(request, "ss/url.html", {"url": shortUrl.url, "short":hash(id)})
+        return render(request, "ss/shorten.html", {"form":urlForm})
+    return render(request, "ss/shorten.html", {"form":UrlForm()})
 
 def hash(id):
     alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
