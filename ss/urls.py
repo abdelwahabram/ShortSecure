de6203585b473b751859b7c1db81d0c6a0ed2367
abdelwahabram <abdelwahab2000r@gmail.com>
2023-02-09 13:32:11 +0200

    added 'redirect' url

--- ss/urls.py
+++ ss/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     path('shorten', views.shorten, name="shorten"),
+    path('<str:short>', views.redirectUrl, name="redirect")
 ]
