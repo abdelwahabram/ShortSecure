d486c4cf713d0b88cc5133d8534cc9f6f3609b94
abdelwahabram <abdelwahab2000r@gmail.com>
2023-02-12 12:08:22 +0200

    added register url

--- ss/urls.py
+++ ss/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('shorten', views.shorten, name="shorten"),
+    path('accounts/register', views.register, name="register"),
     path('<str:short>', views.redirectUrl, name="redirect")
 ]
