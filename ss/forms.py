d4788714bfaf475af64c4105f28399eaf4cba387
abdelwahabram <abdelwahab2000r@gmail.com>
2023-02-12 12:08:37 +0200

    created user registeration form

--- ss/forms.py
+++ ss/forms.py
@@ -1,7 +1,16 @@
-from .models import Url
-from django.forms import ModelForm
+from django import forms
+from .models import Url, User
+# from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 
-class UrlForm(ModelForm):
+class UrlForm(forms.ModelForm):
     class Meta():
         model = Url
         fields = ['url']
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
