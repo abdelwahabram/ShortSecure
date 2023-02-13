0769f80f2db3eead86618209a9477e462add9a24
abdelwahabram <abdelwahab2000r@gmail.com>
2023-02-13 10:53:37 +0200

    added 'url' pattern

--- ss/urls.py
+++ ss/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('shorten', views.shorten, name="shorten"),
     path('urls/', views.viewUrls, name="urls"),
+    path('url/<int:id>', views.viewUrl, name="url"),
     path('accounts/register', views.register, name="register"),
     path('<str:short>', views.redirectUrl, name="redirect")
 ]
